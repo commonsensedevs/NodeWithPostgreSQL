0129ac246143eca534db6e2d2ec8b5a9e423e767
Gomti Mehta <gomtiprofessional@gmail.com>
2023-09-17 15:42:27 -0500

    Routing

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const path = require('path');
 const app = express();
 const ejs = require('ejs');
 const port = 3000;
-
-
+const sessionRouter = express.Router();
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+sessionRouter.route('/').get((req, res) => {
+    res.send('Routing successful');
+});
+
+sessionRouter.route('/1').get((req, res) => {
+    res.send('Single session routing successful');
+});
+
+app.use('/sessions', sessionRouter);
 
 app.get('/', (req, res) =>{
     res.render('index', {title: 'Home', data: [{name: 'Gomti', gender: 'Woman'}, {name: 'Flavio', gender: 'Man'}, {name: 'Parth', gender: 'Man'}]});
